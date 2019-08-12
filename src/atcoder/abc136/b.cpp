df2c809a3014e541ffa78adc739ce2e699fdb2ea
kazumatsudo <kazuma.matsudo@icloud.com>
2019-08-12 20:13:05 +0900

    abc136/b

---
+++ src/atcoder/abc136/b.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // 入力
+    int a;
+    cin >> a;
+
+    if (a < 9) {
+        cout << a << endl;
+    } else if (a < 100) {
+        cout << 9 << endl;
+    } else if (a < 1000) {
+        int a100 = a - 100;
+        cout << 10 + a100 << endl;
+    } else if (a < 10000) {
+        cout << 909 << endl;
+    } else if (a < 100000) {
+        int a10000 = a - 10000;
+        cout << 910 + a10000 << endl;
+    } else {
+        cout << 90909 << endl;
+    }
+
+    // 出力
+    return 0;
+}
+
