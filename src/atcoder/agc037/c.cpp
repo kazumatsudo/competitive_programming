e6a94299da5ff01fad19af8d0089bb7d436478c4
kazumatsudo <kazuma.matsudo@icloud.com>
2019-08-17 20:04:03 +0900

    create agc037

---
+++ src/atcoder/agc037/c.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    // 入力
+    int a,b;
+    cin >> a >> b;
+
+    // 出力
+    cout << a + b << endl;
+    return 0;
+}
+
