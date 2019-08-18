30d126a80f261c104d8417b833f6c72854526dbb
kazumatsudo <kazuma.matsudo@icloud.com>
2019-08-18 23:43:44 +0900

    abc138/d

--- src/atcoder/abc138/d.cpp
+++ src/atcoder/abc138/d.cpp
@@ -2,15 +2,48 @@
 #define rep(i,n) for (int i = 0; i < (n); ++i)
 using namespace std;
 typedef long long ll;
+typedef pair<int, int> P;
+
+vector<vector<int>> n(200005);
+vector<ll> counter(200005, 0);
+map<int, ll> sum = {};
+
+void dfs(int first, ll second) {
+    for (int v: n[first]) {
+        dfs(v, second + sum[v]);
+    }
+
+    counter[first] += second;
+}
 
 int main()
 {
     // 入力
-    int a;
-    cin >> a;
+    int N, Q;
+    cin >> N >> Q;
+
+    for (int i = 1; i < N; i++) {
+        int a, b;
+        cin >> a >> b;
+
+        n[a].push_back(b);
+    }
+
+    for (int i = 1; i <= Q; i++) {
+        int p, x;
+        cin >> p >> x;
+
+        sum[p] = sum[p] + x;
+    }
+
+    auto i = sum.begin();
+    dfs(1, sum[1]);
 
     // 出力
-    cout << a << endl;
+    for (int i = 1; i <= N; i++) {
+        printf("%lld%c", counter[i], i == N ? '\n' : ' ');
+    }
+
     return 0;
 }
 
