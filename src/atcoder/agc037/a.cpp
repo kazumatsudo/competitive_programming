8445c84857e2c798ce240aabd0319da1becffbef
kazumatsudo <kazuma.matsudo@icloud.com>
2019-08-18 12:03:14 +0900

    agc037/a

--- src/atcoder/agc037/a.cpp
+++ src/atcoder/agc037/a.cpp
@@ -6,11 +6,23 @@ typedef long long ll;
 int main()
 {
     // 入力
-    int a,b;
-    cin >> a >> b;
+    string S;
+    cin >> S;
+
+    string prevS = "";
+    string currentS = "";
+    int k = 0;
+    rep (i, S.size()) {
+        currentS = currentS + S[i];
+        if (prevS != currentS) {
+            prevS = currentS;
+            currentS = "";
+            k++;
+        }
+    }
 
     // 出力
-    cout << a + b << endl;
+    cout << k << endl;
     return 0;
 }
 
