c360922833bcffe2bb2633737e8b97ef095b0b83
kazumatsudo <kazuma.matsudo@icloud.com>
2019-08-21 17:14:59 +0900

    マクロに #define sz(x) int(x.size()) を追加

--- src/atcoder/create_contest_directory.sh
+++ src/atcoder/create_contest_directory.sh
@@ -9,16 +9,15 @@ fi
 
 echo "#include <bits/stdc++.h>
 #define rep(i,n) for (int i = 0; i < (n); ++i)
+#define sz(x) int(x.size())
 using namespace std;
 typedef long long ll;
 
 int main()
 {
-    // 入力
     int a;
     cin >> a;
 
-    // 出力
     cout << a << endl;
     return 0;
 }
