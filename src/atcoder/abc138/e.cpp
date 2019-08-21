65fac28cd9c8b912d0f35f031c62a3b42e146854
kazumatsudo <kazuma.matsudo@icloud.com>
2019-08-21 17:53:19 +0900

    abc138/e

--- src/atcoder/abc138/e.cpp
+++ src/atcoder/abc138/e.cpp
@@ -1,16 +1,39 @@
 #include <bits/stdc++.h>
 #define rep(i,n) for (int i = 0; i < (n); ++i)
+#define sz(x) int(x.size())
 using namespace std;
 typedef long long ll;
 
 int main()
 {
-    // 入力
-    int a;
-    cin >> a;
+    string s, t;
+    cin >> s >> t;
+    int n = sz(s), m = sz(t);
+    vector<vector<int>> is(26);
+    rep (i, n) {
+        is[s[i] - 'a'].push_back(i);
+    }
+    rep (i, n) {
+        is[s[i] - 'a'].push_back(i + n);
+    }
 
-    // 出力
-    cout << a << endl;
+    ll ans = 0;
+    int p = 0;
+    rep (i, m) {
+        int c = t[i] - 'a';
+        if (sz(is[c]) == 0) {
+            puts("-1");
+            return 0;
+        }
+
+        p = *lower_bound(is[c].begin(), is[c].end(), p) + 1;
+        if (p >= n) {
+            p -= n;
+            ans += n;
+        }
+    }
+
+    ans += p;
+    cout << ans << endl;
     return 0;
 }
-
