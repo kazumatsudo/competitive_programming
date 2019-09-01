f8127171fc049cc092096c7940a0d3fedd80804c
kazumatsudo <kazuma.matsudo@icloud.com>
2019-09-01 23:14:30 +0900

    abc139/d

---
+++ src/atcoder/abc139/d.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define sz(x) int(x.size())
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    ll N;
+    cin >> N;
+
+    cout << N * (N - 1) / 2 << endl;
+    return 0;
+}
+
