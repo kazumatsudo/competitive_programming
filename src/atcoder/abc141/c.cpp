60ebf214c6893540324881e9cf912d7d1846db44
kazumatsudo <kazuma.matsudo@icloud.com>
2019-09-16 08:12:37 +0900

    abc141/c

--- src/atcoder/abc141/c.cpp
+++ src/atcoder/abc141/c.cpp
@@ -6,10 +6,19 @@ typedef long long ll;
 
 int main()
 {
-    int a;
-    cin >> a;
+    int N, K, Q;
+    cin >> N >> K >> Q;
 
-    cout << "abc141/c" << endl;
+    vector<int> answer_man(N, 0);
+    rep (i, Q) {
+        int answer;
+        cin >> answer;
+        answer_man[answer - 1]++;
+    }
+
+    rep (i, N) {
+        cout << (K - Q + answer_man[i] > 0 ? "Yes" : "No") << endl;
+    }
+
     return 0;
 }
-
