bd029894ca5226160de00f219c3ba981d1044cfd
kazumatsudo <kazuma.matsudo@icloud.com>
2019-09-16 08:13:16 +0900

    abc141/e

--- src/atcoder/abc141/e.cpp
+++ src/atcoder/abc141/e.cpp
@@ -6,10 +6,28 @@ typedef long long ll;
 
 int main()
 {
-    int a;
-    cin >> a;
+    int N;
+    cin >> N;
 
-    cout << "abc141/e" << endl;
+    string S;
+    cin >> S;
+
+    unsigned long len = 0;
+    for (unsigned long startL1 = 0; startL1 + (len * 2) < N; startL1++) {
+        for (unsigned long startL2 = startL1 + 1; startL2 + len < N; startL2++) {
+            for (unsigned long end = len; end < startL2 - startL1; end++) {
+                string s1 = S.substr(startL1, end + 1);
+                string s2 = S.substr(startL2, end + 1);
+
+                if (s1 == s2) {
+                    len = max(len, s1.size());
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    cout << len << endl;
     return 0;
 }
-
