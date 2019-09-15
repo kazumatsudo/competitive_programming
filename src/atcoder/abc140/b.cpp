d04a9e38b1a1fd1c5095a6759e5c9b927aaa9996
kazumatsudo <kazuma.matsudo@icloud.com>
2019-09-15 20:27:56 +0900

    abc140/b

--- src/atcoder/abc140/b.cpp
+++ src/atcoder/abc140/b.cpp
@@ -6,10 +6,30 @@ typedef long long ll;
 
 int main()
 {
-    int a;
-    cin >> a;
+    int N;
+    cin >> N;
 
-    cout << a << endl;
+    vector<int> A(N), B(N), C(N - 1);
+    rep (i, N) {
+        cin >> A[i];
+    }
+    rep (i, N) {
+        cin >> B[i];
+    }
+    rep (i, N - 1) {
+        cin >> C[i];
+    }
+
+    int ans = 0;
+    rep (i, N) {
+        ans += B[A[i] - 1];
+
+        if (i != 0 && A[i] - 1 == A[i - 1]) {
+            ans += C[A[i - 1] - 1];
+        }
+    }
+
+    cout << ans << endl;
     return 0;
 }
 
