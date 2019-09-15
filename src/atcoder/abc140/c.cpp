527c127d8d9f6d94dafd22bde74292e8dc7520e3
kazumatsudo <kazuma.matsudo@icloud.com>
2019-09-15 20:28:16 +0900

    abc140/c

--- src/atcoder/abc140/c.cpp
+++ src/atcoder/abc140/c.cpp
@@ -6,10 +6,26 @@ typedef long long ll;
 
 int main()
 {
-    int a;
-    cin >> a;
+    int N;
+    cin >> N;
 
-    cout << a << endl;
+    vector<int> B(N - 1);
+    rep(i, N - 1) {
+        cin >> B[i];
+    }
+    reverse(B.begin(), B.end());
+
+    int ans = 0;
+    rep (i, N - 1) {
+        if (i == 0){
+            ans += B[i];
+        } else {
+            ans += min(B[i], B[i - 1]);
+        }
+    }
+    ans += B[N - 2];
+
+    cout << ans << endl;
     return 0;
 }
 
