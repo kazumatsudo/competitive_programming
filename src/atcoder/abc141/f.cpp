c7dd82f1d04a83a51b522ae84bc2def7f34ade92
kazumatsudo <kazuma.matsudo@icloud.com>
2019-09-15 20:56:02 +0900

    abc141 init

---
+++ src/atcoder/abc141/f.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define sz(x) int(x.size())
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    int a;
+    cin >> a;
+
+    cout << "abc141/f" << endl;
+    return 0;
+}
+
