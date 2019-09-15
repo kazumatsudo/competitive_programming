6a396f8c9f3d02939eb2a372308ee19f26bf2fab
kazumatsudo <kazuma.matsudo@icloud.com>
2019-09-15 20:29:30 +0900

    abc140/e

--- src/atcoder/abc140/e.cpp
+++ src/atcoder/abc140/e.cpp
@@ -1,15 +1,57 @@
+// wrong_answer
 #include <bits/stdc++.h>
 #define rep(i,n) for (int i = 0; i < (n); ++i)
 #define sz(x) int(x.size())
 using namespace std;
 typedef long long ll;
 
+typedef pair<int, int> firstSecond;
+
 int main()
 {
-    int a;
-    cin >> a;
+    int N;
+    cin >> N;
 
-    cout << a << endl;
+    vector<int> P(N);
+    rep (i, N) {
+        cin >> P[i];
+    }
+
+    int ans = 0;
+    vector<vector<firstSecond>> dp(N, vector<firstSecond>(N, firstSecond(-1, -1)));
+    for (int i = 0; i < N - 1; i++) {
+        for (int j = i + 1; j < N; j++) {
+            if (i + 1 == j) {
+                int firstCandidate, secondCandidate;
+                if (P[i] > P[j]) {
+                    firstCandidate = P[i];
+                    secondCandidate = P[j];
+                } else {
+                    firstCandidate = P[j];
+                    secondCandidate = P[i];
+                }
+
+                dp[i][j] = firstSecond(firstCandidate, secondCandidate);
+            } else {
+                int current = P[j];
+
+                int firstPrev = dp[i][j - 1].first;
+                int secondPrev =  dp[i][j - 1].second;
+
+                if (secondPrev > current) {
+                    dp[i][j] = dp[i][j - 1];
+                } else if (current > firstPrev) {
+                    dp[i][j] = firstSecond(current, firstPrev);
+                } else {
+                    dp[i][j] = firstSecond(firstPrev, current);
+                }
+            }
+
+            ans += dp[i][j].second;
+        }
+    }
+
+    cout << ans << endl;
     return 0;
 }
 
